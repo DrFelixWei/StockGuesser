6c212340eca2d40bc5e83e28c60cb0a8444bb6fe
Felix Wei <felixwei7@gmail.com>
2024-08-03 11:41:02 -0700

    added dategenerated col to stocksnapshot

--- api/prisma/migrations/20240803153828_add_stock_snapshot_table/migration.sql
+++ api/prisma/migrations/20240803153828_add_stock_snapshot_table/migration.sql
@@ -3,6 +3,7 @@ CREATE TABLE "StockSnapshot" (
     "id" SERIAL NOT NULL,
     "symbol" TEXT NOT NULL,
     "name" TEXT NOT NULL,
+    "dateGenerated" TIMESTAMPTZ NOT NULL,
     "prices" JSONB NOT NULL,
 
     CONSTRAINT "StockSnapshot_pkey" PRIMARY KEY ("id")
