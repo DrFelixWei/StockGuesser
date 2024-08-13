32c8df5c3aaaf9ee48bcb498fda93c6acd34daac
Felix Wei <felixwei7@gmail.com>
2024-08-12 17:51:10 -0700

    Revert "added apollo client"
    This reverts commit 474f4061318f5da1f04f4b2489da6abb0619dbf6.

--- webapp/src/components/StockData.jsx
+++ webapp/src/components/StockData.jsx
@@ -1,29 +1,9 @@
 import React, { useState } from 'react';
 import { Button } from '@mui/material';
-import { useQuery, useMutation, useApolloClient } from '@apollo/client'
 
 const StockData = () => {
-  const client = useApolloClient()
   const [data, setData] = useState(null);
   const [loading, setLoading] = useState(false);
-
-  const onSubmit = async ({ amount }) => {
-    try {
-      await client.mutate({
-        mutation: mutations.stock.createStock,
-        variables: {
-          amount: amount,
-        },
-      })
-      // openDialog()
-    } catch (e) {
-      setAlert({
-        message: `Failed to generate stock snapshot: ${e.message ?? ''}`,
-        error: e.message,
-        severity: 'error',
-      })
-    }
-  }
 
   const fetchData = async () => {
     setLoading(true);
@@ -32,8 +12,6 @@ const StockData = () => {
     setData(result);
     setLoading(false);
   };
-
-  
 
   return (
     <div>
