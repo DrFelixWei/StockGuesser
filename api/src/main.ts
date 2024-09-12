fbb11ff09325d73c75a8be8d59265bf5027f11fa
Felix Wei <felixwei7@gmail.com>
2024-09-11 22:34:50 -0700

    temp CORS test

--- api/src/main.ts
+++ api/src/main.ts
@@ -7,14 +7,10 @@ async function bootstrap() {
   
   const configService = app.get(ConfigService);
   let allowedOrigins = configService.get<string>('ALLOWED_ORIGINS').split(',');
-  allowedOrigins.push('http://stockguesserwebapp-production.up.railway.app');
-  allowedOrigins.push('http://stockguesserapi-production.up.railway.app');
-  allowedOrigins.push('https://stockguesserwebapp-production.up.railway.app');
-  allowedOrigins.push('https://stockguesserapi-production.up.railway.app');
   console.log("allowedOrigins: ", allowedOrigins);
 
   app.enableCors({
-    origin: allowedOrigins,
+    origin: true,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
   });
